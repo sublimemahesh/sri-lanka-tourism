f1838d87571284b2eb02858f7ecab47f9a58731b
mayomi <mayomi@sublime.lk>
2018-02-09 10:11:59 +0530

    #20 Manage tour packages

---
+++ admin/create-tour-package.php
@@ -0,0 +1,173 @@
+<?php
+include_once(dirname(__FILE__) . '/../class/include.php');
+include_once(dirname(__FILE__) . './auth.php');
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <title>Add New Tour Tour Package - www.srilankatourism.travel</title>
+        <!-- Favicon-->
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+        <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+        <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+
+        <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+
+        <link href="plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+
+        <link href="css/style.css" rel="stylesheet">
+
+        <link href="css/themes/all-themes.css" rel="stylesheet" />
+    </head>
+
+    <body class="theme-red">
+        <?php
+        include './navigation-and-header.php';
+        ?> 
+        <section class="content">
+            <div class="container-fluid"> 
+                <?php
+                $vali = new Validator();
+
+                $vali->show_message();
+                ?>
+                <!-- Vertical Layout -->
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <div class="header">
+                                <h2>Add New Tour Package</h2>
+                                <ul class="header-dropdown">
+                                    <li class="">
+                                        <a href="manage-tour-package.php">
+                                            <i class="material-icons">list</i> 
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="body">
+                                <form class="form-horizontal"  method="post" action="post-and-get/tour-package.php" enctype="multipart/form-data"> 
+                                    <div class="row clearfix">
+                                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                            <label for="name">Name</label>
+                                        </div>
+                                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                            <div class="form-group">
+                                                <div class="form-line">
+                                                    <input type="text" id="name" class="form-control" placeholder="Enter name" autocomplete="off" name="name" required="TRUE">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                    <div class="row clearfix">
+                                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                            <label for="price">Price</label>
+                                        </div>
+                                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                            <div class="form-group">
+                                                <div class="form-line">
+                                                    <input type="text" id="price" class="form-control" placeholder="Enter price" autocomplete="off" name="price" required="TRUE">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div> 
+
+                                    <div class="row clearfix">
+                                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                            <label for="picture_name">Picture</label>
+                                        </div>
+                                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7"> 
+                                            <div class="form-group form-float">
+                                                <div class="form-line">
+                                                    <input type="file" id="picture_name" class="form-control" name="picture_name"  required="true">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row clearfix">
+                                        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                            <label for="description">Description</label>
+                                        </div>
+                                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                            <div class="form-line">
+                                                <textarea id="description" name="description" class="form-control" rows="5"></textarea> 
+                                                <input type="hidden" value="1" name="active" />
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="row clearfix">
+                                        <div class="col-lg-offset-2 col-md-offset-2 col-sm-offset-4 col-xs-offset-5"> 
+                                            <input type="submit" name="add-tour-package" class="btn btn-primary m-t-15 waves-effect" value="Add tour package"/>
+                                        </div>
+                                    </div>
+                                    <hr/>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <!-- Jquery Core Js -->
+        <script src="plugins/jquery/jquery.min.js"></script>
+
+        <!-- Bootstrap Core Js -->
+        <script src="plugins/bootstrap/js/bootstrap.js"></script>
+
+        <!-- Select Plugin Js -->
+        <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+        <!-- Slimscroll Plugin Js -->
+        <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+        <!-- Waves Effect Plugin Js -->
+        <script src="plugins/node-waves/waves.js"></script>
+
+        <!-- Custom Js -->
+        <script src="js/admin.js"></script>
+
+        <!-- Demo Js -->
+        <script src="js/demo.js"></script>
+        <script src="tinymce/js/tinymce/tinymce.min.js"></script>
+        <script>
+            tinymce.init({
+                selector: "#description",
+                // ===========================================
+                // INCLUDE THE PLUGIN
+                // ===========================================
+
+                plugins: [
+                    "advlist autolink lists link image charmap print preview anchor",
+                    "searchreplace visualblocks code fullscreen",
+                    "insertdatetime media table contextmenu paste"
+                ],
+                // ===========================================
+                // PUT PLUGIN'S BUTTON on the toolbar
+                // ===========================================
+
+                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages",
+                // ===========================================
+                // SET RELATIVE_URLS to FALSE (This is required for images to display properly)
+                // ===========================================
+
+                relative_urls: false
+
+            });
+
+
+        </script>
+    </body>
+
+</html>
