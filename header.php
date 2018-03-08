8ad2fb8243689e3fd23dc9f5186332c864f0af4a
srisandamali <iresha@sublime.lk>
2018-03-08 10:49:28 +0530

    #85 Front-End : Transportation Design

---
+++ header.php
@@ -0,0 +1,86 @@
+<header class="main-header">
+            <div class="container clearfix common-pad">
+                <div class="row contact-head">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 contact-row">
+                        <div class="contact-detail">
+                            <i class="fa fa-phone"><span class="details">0757522415</span></i><span class="separate">||</span>
+                            <i class="fa fa-envelope-o"><span class="details">srilankatourism.com</span></i>
+                            <a href="member/login.php" ><img class="add-user "src="images/icon/user(1).png" alt="" /></a>
+                            <a href="member/register.php"><img class="index" src="images/icon/user(2).png" alt=""/></a>
+                        </div>
+                    </div>
+                </div>
+                <hr style="padding-top: -10px !important;">
+                <!--                size-xs-->
+                <div class="row logo-section">
+                    <div class="col-sm-4 col-xs-4  hidden-md hidden-lg hidden-sm visible-xs">
+                        <img src="images/logo-intro2.png" alt="image" class="logo"/>
+                    </div>
+                    <div class="mobile-frnd  col-sm-8 col-xs-8 hidden-md hidden-lg hidden-sm visible-xs">
+                        <div class="sec-header sec-header-pad">
+                            <h2>Sri Lanka Tourism</h2>
+                            <h3>Plan your trip to Sri Lanka</h3>
+                        </div>
+                        <div class=" icon-row">
+                            <div class="sec-header sec-header-pad">
+                                <ul class="social-icon">
+                                    <h4 class="hidden">Follow us</h4>
+                                    <a data-original-title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
+                                    <a data-original-title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
+                                    <a data-original-title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                                    <a data-original-title="instagram" href="#"><i class="fa fa-instagram"></i></a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!--                size-md/lg-->
+                <div class="row logo-section hidden-sm hidden-xs">
+                    <div class="col-lg-5 col-md-4 col-sm-12 col-xs-12">
+                        <div class="sec-header sec-header-pad">
+                            <h2>Sri Lanka Tourism</h2>
+                            <h3>Plan your trip to Sri Lanka</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4 hidden-sm hidden-xs">
+                        <img src="images/logo-intro2.png" alt="image" class="logo"/>
+                    </div>
+                    <div class="col-lg-5 col-md-4  icon-row">
+                        <div class="sec-header sec-header-pad">
+                            <ul class="social-icon">
+                                <h4>Follow us</h4>
+                                <a data-original-title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
+                                <a data-original-title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
+                                <a data-original-title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                                <a data-original-title="instagram" href="#"><i class="fa fa-instagram"></i></a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!--                size-sm-->
+                <div class="row logo-section hidden-md hidden-lg hidden-xs visible-sm">
+
+                    <div class=" col-sm-4">
+                        <div class="sec-header sec-header-pad">
+                            <h2>Sri Lanka Tourism</h2>
+                            <h3>Plan your trip to Sri Lanka</h3>
+                        </div>
+                    </div>
+                    <div class=" col-sm-4">
+                        <img src="images/logo-intro2.png" alt="image" class="logo"/>
+                    </div>
+                    <div class="  col-sm-4  icon-row">
+                        <div class="sec-header sec-header-pad">
+                            <ul class="social-icon">
+                                <h4>Follow us</h4>
+                                <a data-original-title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
+                                <a data-original-title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
+                                <a data-original-title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                                <a data-original-title="instagram" href="#"><i class="fa fa-instagram"></i></a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
