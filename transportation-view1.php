a88c3a32df1168ee69310cf4ac4c9ae1b34ad4f6
Dhanusha <dhanusha@sublime.lk>
2018-04-11 17:17:32 +0530

    #207 Transport view corrections

--- transportation-view1.php
+++ transportation-view1.php
@@ -8,7 +8,6 @@ $VIEW_TRANSPORTS = new Transports($id);
 $TRANSPORTS = new Transports($id);
 $TRANSPORTS_PHOTO = new TransportPhoto(NULL);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -31,11 +30,17 @@ $TRANSPORTS_PHOTO = new TransportPhoto(NULL);
         <?php
         include './header.php';
         ?>
-
         <div class="row" style="background-color: #fff;">
             <div class="container transport-container">
-                <div class="col-md-8 p pro-details">
-                    <div class="row">
+                <?php
+                $vali = new Validator();
+                $vali->show_message();
+                ?>
+
+                <div class="row">
+                    <div class="col-md-8 p pro-details">
+
+
                         <div id="transport_photos" class="galleria-slider">
                             <?php
                             $VIEW_TRANSPORT = TransportPhoto::getTransportPhotosById($id);
@@ -55,6 +60,7 @@ $TRANSPORTS_PHOTO = new TransportPhoto(NULL);
                                 <?php echo $VIEW_TRANSPORTS->description; ?>
                             </span>
                         </div>
+
                         <div id="myCarousel" class="carousel slide" data-ride="carousel">
                             <h2 class="t-comment">Customer <b>Testimonials</b></h2>
                             <!-- Carousel indicators -->
@@ -165,111 +171,111 @@ $TRANSPORTS_PHOTO = new TransportPhoto(NULL);
                             include './add-feedback.php';
                             ?>
                         </div>
+
                     </div>
-                </div>
-                <div class="col-md-4 ">
-                    <div class="right-side">
-                        <div class="transport-facilities details-vehical">
-                            <div class="row">
-                                <div class=" ">
-                                    <h3 class="subtitle fancy"><span>Lorem</span></h3>
-                                    <div class="car-type text-uppercase"><strong> <?php echo $VIEW_TRANSPORTS->title; ?></strong></div>
-                                    <div class="car-name"><strong> <?php echo $CONDITION->name; ?></strong></div>
-                                    <div class="row facility-list">
-                                        <div class="col-md-2 ">
-                                            <img src="assets/img/transport/people.png" class="facility-img">
-                                            <span><?php echo $VIEW_TRANSPORTS->no_of_passangers; ?></span>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <img src="assets/img/transport/luggage.png"class="facility-img">
-                                            <span><?php echo $VIEW_TRANSPORTS->no_of_baggages; ?></span>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <img src="assets/img/transport/door.png"class="facility-img">
-                                            <span><?php echo $VIEW_TRANSPORTS->no_of_doors; ?></span>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <?php
-                                            if ($TRANSPORTS->ac == 1) {
-                                                ?>
-                                                <img src="assets/img/transport/snow.png"class="facility-img">
-                                                <span>Air Conditioned  </span>
+                    <div class="col-md-4 ">
+                        <div class="right-side">
+                            <div class="transport-facilities details-vehical">
+                                <div class="row">
+                                    <div class=" ">
+                                        <h3 class="subtitle fancy"><span>Lorem</span></h3>
+                                        <div class="car-type text-uppercase"><strong> <?php echo $VIEW_TRANSPORTS->title; ?></strong></div>
+                                        <div class="car-name"><strong> <?php echo $CONDITION->name; ?></strong></div>
+                                        <div class="row facility-list">
+                                            <div class="col-md-2 ">
+                                                <img src="assets/img/transport/people.png" class="facility-img">
+                                                <span><?php echo $VIEW_TRANSPORTS->no_of_passangers; ?></span>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img src="assets/img/transport/luggage.png"class="facility-img">
+                                                <span><?php echo $VIEW_TRANSPORTS->no_of_baggages; ?></span>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img src="assets/img/transport/door.png"class="facility-img">
+                                                <span><?php echo $VIEW_TRANSPORTS->no_of_doors; ?></span>
+                                            </div>
+                                            <div class="col-md-6">
                                                 <?php
-                                            } else {
+                                                if ($TRANSPORTS->ac == 1) {
+                                                    ?>
+                                                    <img src="assets/img/transport/snow.png"class="facility-img">
+                                                    <span>Air Conditioned  </span>
+                                                    <?php
+                                                } else {
+                                                    ?>
+                                                    <img src="assets/img/transport/snow.png"class="facility-img">
+                                                    <span>Non Air Conditioned </span>
+                                                    <?php
+                                                }
                                                 ?>
-                                                <img src="assets/img/transport/snow.png"class="facility-img">
-                                                <span>Non Air Conditioned </span>
+                                            </div>
+                                        </div>
+                                        <div class="row car-details ">
+
+                                            <div class="col-md-12 fuel-type">
+                                                <span><strong>Fuel Type:</strong> <?php echo $FUEL_TYPE->name; ?> </span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="more-transport">
+                                <div class="row" >
+                                    <h3 class="subtitle fancy"><span>Other Transport</span></h3>
+
+                                    <?php
+                                    $OTHER_TRANSPORT = Transports::all();
+                                    foreach ($OTHER_TRANSPORT as $other_transports) {
+                                        ?>
+                                        <div class="car-pack col-md-12">
+                                            <div class="col-md-4 hover01">
                                                 <?php
-                                            }
-                                            ?>
-                                        </div>
-                                    </div>
-                                    <div class="row car-details ">
+                                                foreach ($TRANSPORTS_PHOTO->getTransportPhotosById($other_transports['id']) as $key => $TRANSPORTS_P) {
+                                                    if ($key == 1) {
+                                                        break;
+                                                    }
+                                                    ?>
+                                                    <figure><img src="upload/transport/thumb/<?php echo $TRANSPORTS_P['image_name']; ?>" alt="" class="img-responsive" style="width: 100px; height: 75px;"/></figure>
+                                                    <?php
+                                                }
+                                                ?>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="transports-desc">
+                                                    <?php echo substr($other_transports ['description'], 0, 75) . '...'; ?>
+                                                </div>
+                                                <div class="t-more text-center">
+                                                    <a href="transportation-view.php?id=<?php echo $other_transports['id']; ?>" class="btn btn-sm blue">
+                                                        <span class="fa fa-eye"></span>View
+                                                    </a>
+                                                </div>
 
-                                        <div class="col-md-12 fuel-type">
-                                            <span><strong>Fuel Type:</strong> <?php echo $FUEL_TYPE->name; ?> </span>
-                                        </div>
-                                    </div>
 
+                                            </div>
+                                        </div>
+
+                                        <?php
+                                    }
+                                    ?>
                                 </div>
                             </div>
                         </div>
-                        <div class="more-transport">
-                            <div class="row" >
-                                <h3 class="subtitle fancy"><span>Other Transport</span></h3>
-
-                                <?php
-                                $OTHER_TRANSPORT = Transports::all();
-                                foreach ($OTHER_TRANSPORT as $other_transports) {
-                                    ?>
-                                    <div class="car-pack col-md-12">
-                                        <div class="col-md-4 hover01">
-                                            <?php
-                                            foreach ($TRANSPORTS_PHOTO->getTransportPhotosById($other_transports['id']) as $key => $TRANSPORTS_P) {
-                                                if ($key == 1) {
-                                                    break;
-                                                }
-                                                ?>
-                                                <figure><img src="upload/transport/thumb/<?php echo $TRANSPORTS_P['image_name']; ?>" alt="" class="img-responsive" style="width: 100px; height: 75px;"/></figure>
-                                                <?php
-                                            }
-                                            ?>
-                                        </div>
-                                        <div class="col-md-8">
-                                            <div class="transports-desc">
-                                                <?php echo substr($other_transports ['description'], 0, 75) . '...'; ?>
-                                            </div>
-                                            <div class="t-more text-center">
-                                                <a href="transportation-view.php?id=<?php echo $other_transports['id']; ?>" class="btn btn-sm blue">
-                                                    <span class="fa fa-eye"></span>View
-                                                </a>
-                                            </div>
-
-
-                                        </div>
-                                    </div>
-
-                                    <?php
-                                }
-                                ?>
-                            </div>
-                        </div>
                     </div>
+
                 </div>
 
-
             </div>
-
         </div>
 
+        <!-- Our Resort Values style-->  
         <?php
         include './footer.php';
         ?>
         <script src="js/jquery-2.2.4.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
-        <script src="js/owl.carousel.min.js" type="text/javascript"></script>
         <script src="js/jquery.flexslider.js" type="text/javascript"></script>
-        <link href="galleria/themes/classic/galleria.classic.css" rel="stylesheet" type="text/css"/>
+        <link href="css/galleria.classic.css" rel="stylesheet" type="text/css"/>
         <script src="js/galleria.js" type="text/javascript"></script>
         <script src="js/galleria.classic.min.js" type="text/javascript"></script>
         <script type="text/javascript">
@@ -280,10 +286,11 @@ $TRANSPORTS_PHOTO = new TransportPhoto(NULL);
                 autoplay: 7000,
                 lightbox: true,
                 showInfo: true,
-
                 //                imageCrop: true,
             });
         </script>
+
+
         <script>
             jQuery(document).ready(function () {
                 jQuery('#btn-add-comment').click(function () {
@@ -326,9 +333,6 @@ $TRANSPORTS_PHOTO = new TransportPhoto(NULL);
                 });
             });
         </script>
-
     </body> 
-
 </html>
-
 
