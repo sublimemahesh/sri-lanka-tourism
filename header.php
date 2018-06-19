6f2e07a26b66745378e00929c65a32087dc5f810
chalanadulaj <chalanadulaj99@gmail.com>
2018-06-19 15:31:34 +0530

    #431 Home page article design correction and header re correction

--- header.php
+++ header.php
@@ -76,102 +76,99 @@
                 }
             } else {
                 ?>
-                <a href="visitor-login.php" ><img class="add-user" src="images/icon/user(1).png" alt="" /></a>
-                <a href="visitor-register.php"><img class="index" src="images/icon/user(2).png" alt=""/></a>
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                    <div class="pull-right">
+                        <a href="visitor-login.php" ><img class="add-user" src="images/icon/user(1).png" alt="" /></a>
+                        <a href="visitor-register.php"><img class="user-t" src="images/icon/user(2).png" alt="" /></a>
+                    </div>
+                </div>
                 <?php
             }
             ?>
         </div>
-    </div>
-</div>
-<hr style="padding-top: -10px !important;">
-<div class="row logo-section">
-    <div class="col-sm-4 col-xs-4  hidden-md hidden-lg hidden-sm visible-xs logo-col">
-        <a href="index.php">
-            <img src="images/logo-intro2.png" alt="image" class="logo"/>
-        </a>
+        <hr style="padding-top: -10px !important;">
+        <div class=" hidden-sm hidden-xs " >
+            <div class="col-md-5 p-o">
+                <div class="sec-header sec-header-pad">
+                    <a class="head-link-hover" href="index.php">
+                        <h2>Sri Lanka Tourism</h2>
+                        <h3>Plan your trip to Sri Lanka</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="hidden-md hidden-lg">
+                <div class="sec-header sec-header-pad">
+                    <a class="head-link-hover" href="index.php">
+                        <h2>Sri Lanka Tourism</h2>
+                        <h3>Plan your trip to Sri Lanka</h3>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-2 ">
+                <a href="index.php">
+                    <img src="images/logo-intro2.png" alt="image" class="logo" id="logo1"/>  
+                </a>
+            </div>
 
-    </div>
-    <div class="mobile-frnd  col-sm-8 col-xs-8 hidden-md hidden-lg hidden-sm visible-xs">
-        <div class="sec-header sec-header-pad">
-            <a class="head-link-hover" href="index.php">
-                <h2>Sri Lanka Tourism</h2>
-
-                <h3>Plan your trip to Sri Lanka</h3>
-            </a>
+            <div class="col-md-3">
+                <div class="sec-header sec-header-pad">
+                    <ul class="social-icon">
+                        <h4>Follow us</h4>
+                        <a data-original-title="Facebook" href="#"><i class="fa fa-facebook index-fa-facebook"></i></a>
+                        <a data-original-title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
+                        <a data-original-title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                        <a data-original-title="instagram" href="#"><i class="fa fa-instagram"></i></a>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="sec-header sec-header-pad ">
+                    <ul>
+                        <li> 
+                            <a class="head-link-hover" href="member/index.php">
+                                <div class="c-button-member-mobile add-your" >
+                                    <span>Add Your Business</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul> 
+                </div>
+            </div>
 
         </div>
-        <div class=" icon-row">
-            <div class="sec-header sec-header-pad">
-                <ul class="social-icon"> 
-                    <li><a data-original-title="Facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a data-original-title="Twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a data-original-title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a data-original-title="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
-                    <li> <a class="head-link-hover" href="member/index.php"><div class="button-member-mobile add-your" ><span>Add Your Business</span></div></a> </li>
-                </ul>
+        <div class=" hidden-lg hidden-md visible-sm visible-xs">
+            <div class="row">
+                <div class=" col-xs-4 col-sm-4 p-o">
+                    <a href="index.php">
+                        <img src="images/logo-intro2.png" alt="image" class="logo"/>  
+                    </a>
+                </div>
+                <div class="col-xs-8 col-sm-8 p-o">
+                    <div class="sec-header sec-header-pad c-sec-header-pad">
+                        <a class="head-link-hover" href="index.php">
+                            <h2>Sri Lanka Tourism</h2>
+                            <h3>Plan your trip to Sri Lanka</h3>
+                        </a>
+                        <br>
+                        <div class="c-padding">
+                            <h4 class="hidden-sm hidden-xs">Follow us</h4>
+                            <a data-original-title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
+                            <a data-original-title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
+                            <a data-original-title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                            <a data-original-title="instagram" href="#"><i class="fa fa-instagram"></i></a>
+
+                            <a href="member/index.php">
+                                <div class="c-button-member-mobile add-your" >
+                                    <span>Add Your Business</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-    </div>
-</div>
-<div class="row logo-section hidden-sm hidden-xs">
-    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
-        <div class="sec-header sec-header-pad">
-            <a class="head-link-hover" href="index.php">
-                <h2>Sri Lanka Tourism</h2>
-                <h3>Plan your trip to Sri Lanka</h3>
-            </a>
-
         </div>
-    </div>
-    <div class="col-lg-2  text-center col-md-2 hidden-sm hidden-xs">
-        <a href="index.php">
-            <img src="images/logo-intro2.png" alt="image" class="logo"/> 
-        </a>
-    </div>
-    <div class="col-lg-3 col-md-3  icon-row">
-        <div class="sec-header sec-header-pad">
-            <ul class="social-icon">
-                <h4>Follow us</h4>
-                <a data-original-title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
-                <a data-original-title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
-                <a data-original-title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
-                <a data-original-title="instagram" href="#"><i class="fa fa-instagram"></i></a>
-            </ul>
         </div>
-    </div>
-    <div class="col-lg-2 col-md-2">
-        <a href="member/index.php"><div class="button-member" style="vertical-align:middle"><span>Add Your Business</span></div></a> 
-    </div>
-</div> 
-<div class="row logo-section hidden-md hidden-lg hidden-xs visible-sm"> 
-    <div class=" col-sm-4">
-        <div class="sec-header sec-header-pad">
-            <a class="head-link-hover" href="index.php">
-                <h2>Sri Lanka Tourism</h2>
-                <h3>Plan your trip to Sri Lanka</h3>
-            </a>
+   
 
-        </div>
-    </div>
-    <div class="col-sm-4">
-        <a href="index.php">
-            <img src="images/logo-intro2.png" alt="image" class="logo"/>  
-        </a>
 
-    </div>
-    <div class="col-sm-4  icon-row">
-        <div class="sec-header sec-header-pad">
-            <ul class="social-icon">
-                <h4>Follow us</h4>
-                <a data-original-title="Facebook" href="#"><i class="fa fa-facebook"></i></a>
-                <a data-original-title="Twitter" href="#"><i class="fa fa-twitter"></i></a>
-                <a data-original-title="Linkedin" href="#"><i class="fa fa-linkedin"></i></a>
-                <a data-original-title="instagram" href="#"><i class="fa fa-instagram"></i></a>
-            </ul>
-        </div>
-    </div>
-</div>
-</div>
-</header>
